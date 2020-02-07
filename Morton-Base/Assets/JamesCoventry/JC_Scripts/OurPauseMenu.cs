bd392e3410dfecb98cad4c1d5a9f7fab78905a96
JamesCoventry <58071982+JamesCoventry@users.noreply.github.com>
2020-02-07 12:34:21 +0000

    Updated

--- Morton-Base/Assets/JamesCoventry/JC_Scripts/OurPauseMenu.cs
+++ Morton-Base/Assets/JamesCoventry/JC_Scripts/OurPauseMenu.cs
@@ -1,8 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PauseMenu : MonoBehaviour
+public class OurPauseMenu : MonoBehaviour
 {
-    
+
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject fadeInPanel;
+    public GameObject InventoryPanel;
+
+
+
+    private void Awake()
+    {
+        if (fadeInPanel != null)
+        {
+            GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+            Destroy(panel, 2);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+
+            InventoryPanel.SetActive(false);
+        }
+    }
+
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu_Scene");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!!!");
+        Application.Quit();
+    }
+
 }
+
+
+
