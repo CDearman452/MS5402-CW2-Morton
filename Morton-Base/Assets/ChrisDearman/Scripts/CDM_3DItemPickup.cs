17e32b1f5e8ad3a5d288d0a1decdce96d9512e93
CDearman452 <58072257+CDearman452@users.noreply.github.com>
2020-02-07 13:55:33 +0000

    Update-PaintingsPuzzle-V1Complete
    Paintings puzzle is fully functional and ready for transition into whitebox scene.

--- Morton-Base/Assets/ChrisDearman/Scripts/CDM_3DItemPickup.cs
+++ Morton-Base/Assets/ChrisDearman/Scripts/CDM_3DItemPickup.cs
@@ -32,7 +32,6 @@ public class CDM_3DItemPickup : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E))
         {
             //---------------------------------------
-            // Dissable the character controller to avoid returning it as a result, then collect raycast data, reactivate the character controller
             ry_sightRay = Camera.main.ScreenPointToRay(Input.mousePosition); // Create Ray
             Debug.DrawRay(ry_sightRay.origin, ry_sightRay.direction, Color.blue); // Debug view of ray in editor
             if (Physics.Raycast(ry_sightRay, out rch_PickupCheck, fl_maxPickupDist, lm_playerExclude)) // If a ray using the above parameters hits something within the specified layer/s and within range return true and fill the raycasthit variable
@@ -57,7 +56,7 @@ public class CDM_3DItemPickup : MonoBehaviour
                 {
                     if (go_held != null) // If an object is held
                     {
-                        go_held.transform.SetParent(null); // Unchild the pickup
+                        go_held.transform.SetParent(rch_PickupCheck.transform.gameObject.transform); // Unchild the pickup
                         GameObject _go_temp = rch_PickupCheck.transform.gameObject; // Referance the interacted object as a temp variable
                         go_held.transform.position = _go_temp.transform.GetChild(0).transform.position; // Move the pickup to the set location
                         go_held.layer = LayerMask.NameToLayer("Default"); // Swap it to the default layermask so that it is no longer on the top render layer
---
+++ Morton-Base/Assets/ChrisDearman/Scripts/CDM_PaintingPuzzleManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CDM_PaintingPuzzleManager : MonoBehaviour
+{
+    //==============================================================================================================
+    // Variables
+    // private
+    public bool bl_puzzleComplete = false;
+    public bool[] bl_locksCompleted;
+    private int in_locksCompleted = 0;
+    //---------------------------------------
+    // public
+    public GameObject[] go_paintings;
+    public GameObject[] go_pendants;
+    //==============================================================================================================
+    // Start is called before the first frame update
+    void Start()
+    {
+        bl_locksCompleted = new bool[3];
+    }
+    //==============================================================================================================
+    // Update is called once per frame
+    void Update()
+    {
+        if (!bl_puzzleComplete)
+        {
+            foreach (GameObject _go in go_pendants)
+            {
+                if (!bl_locksCompleted[Array.IndexOf(go_pendants, _go)] && _go.transform.position == go_paintings[Array.IndexOf(go_pendants, _go)].transform.GetChild(0).transform.position)
+                {
+                    _go.GetComponent<Collider>().enabled = false;
+                    go_paintings[Array.IndexOf(go_pendants, _go)].transform.tag = "Untagged";
+                    bl_locksCompleted[Array.IndexOf(go_pendants, _go)] = true;
+                }
+            }
+            in_locksCompleted = 0;
+            foreach (bool _bl in bl_locksCompleted)
+            {
+                if (_bl) in_locksCompleted++;
+            }
+            if (in_locksCompleted >= 3) bl_puzzleComplete = true;
+        }
+        else
+        {
+            go_paintings[1].GetComponent<Animator>().SetBool("bl_open", true);
+            GetComponent<CDM_PaintingPuzzleManager>().enabled = false;
+        }
+    }
+}
