667f3724ff9cce32465cc2ac6d7d417987701c72
JamesCoventry <58071982+JamesCoventry@users.noreply.github.com>
2020-02-07 12:31:41 +0000

    Added menu/pause scripts

---
+++ Morton-Base/Assets/JamesCoventry/JC_Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject fadeInPanel;
+
+    private void Awake()
+    {
+        if (fadeInPanel != null)
+        {
+            GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+            Destroy(panel, 2);
+        }
+    }
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu_Scene");
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
+
+
