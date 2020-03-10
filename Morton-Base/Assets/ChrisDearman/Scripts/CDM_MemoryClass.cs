04769c253d08fc9fe9f11e10afa6722cc07329b3
CDearman452 <58072257+CDearman452@users.noreply.github.com>
2020-03-10 16:30:49 +0000

    Update-SeedPuzzleFinalDraft
    Seed Puzzle completed but needs testing and addition of completed assets

---
+++ Morton-Base/Assets/ChrisDearman/Scripts/CDM_MemoryClass.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Memory
+{
+    public int iterations;
+    public float timeLength;
+    public Vector3 playerPos;
+    public AudioClip sound;
+    public Material skyBox;
+
+    public Memory(int Iterations, AudioClip Sound)
+    {
+        iterations = Iterations;
+        sound = Sound;
+        timeLength = sound.length;
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        skyBox = RenderSettings.skybox;
+    }
+
+}
