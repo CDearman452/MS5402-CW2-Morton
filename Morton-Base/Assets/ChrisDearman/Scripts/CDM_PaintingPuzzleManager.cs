d7f51c9a8af1d60723b2baafef655cfd62611bfb
CDearman452 <58072257+CDearman452@users.noreply.github.com>
2020-03-12 17:43:30 +0000

    Update-Painting-Puzzle Memory and Door
    Painting Puzzle is now at it's final level of functionality.

--- Morton-Base/Assets/ChrisDearman/Scripts/CDM_PaintingPuzzleManager.cs
+++ Morton-Base/Assets/ChrisDearman/Scripts/CDM_PaintingPuzzleManager.cs
@@ -2,24 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
 
 public class CDM_PaintingPuzzleManager : MonoBehaviour
 {
     //==============================================================================================================
     // Variables
     // private
-    public bool bl_puzzleComplete = false;
-    public bool[] bl_locksCompleted;
+    [SerializeField] private bool bl_puzzleComplete = false;
+    private bool[] bl_locksCompleted;
+    private bool bl_memComplete = false;
+    private bool bl_fadeOut;
+    private bool bl_fadeIn;
+    private bool bl_waitForAudio = false;
     private int in_locksCompleted = 0;
+    private int in_num = 0;
+    private GameObject go_blackOut;
+    private GameObject go_player;
+    private Memory mem_01;
+    private AudioSource as_memory;
     //---------------------------------------
     // public
     public GameObject[] go_paintings;
     public GameObject[] go_pendants;
+    public GameObject[] go_worldSets;
+    public GameObject go_memoryObject;
+    public GameObject go_door;
+    public AudioClip ac_memAudio;
+    public Material mt_memorySky;
+    public Light lt_memorySpot;
+    public float fl_fadeDuration;
     //==============================================================================================================
     // Start is called before the first frame update
     void Start()
     {
         bl_locksCompleted = new bool[3];
+        mem_01 = new Memory(1, ac_memAudio);
+        go_blackOut = GameObject.Find("BlackOut");
+        as_memory = GameObject.Find("AudioSource").GetComponent<AudioSource>();
+        go_player = GameObject.FindGameObjectWithTag("Player");
     }
     //==============================================================================================================
     // Update is called once per frame
@@ -42,12 +63,127 @@ public class CDM_PaintingPuzzleManager : MonoBehaviour
             {
                 if (_bl) in_locksCompleted++;
             }
-            if (in_locksCompleted >= 3) bl_puzzleComplete = true;
+
+            if (in_locksCompleted >= 3)
+            {
+                bl_puzzleComplete = true;
+                bl_fadeOut = true;
+            }
         }
         else
         {
-            go_paintings[1].GetComponent<Animator>().SetBool("bl_open", true);
-            GetComponent<CDM_PaintingPuzzleManager>().enabled = false;
+            if (go_paintings[1].GetComponent<Animator>().GetBool("bl_open") != true) go_paintings[1].GetComponent<Animator>().SetBool("bl_open", true);
+            
+            if (!bl_memComplete)
+            {
+                if (bl_fadeOut)
+                {
+                    float fl_fadeValue = (1 / fl_fadeDuration) * Time.deltaTime;
+                    Color cl_newFade = go_blackOut.GetComponent<Image>().color;
+
+                    if (go_blackOut.GetComponent<Image>().color.a < 1)
+                    {
+                        cl_newFade.a += fl_fadeValue;
+                        go_blackOut.GetComponent<Image>().color = cl_newFade;
+                    }
+                    else
+                    {
+                        Debug.Log("Check");
+                        bl_fadeOut = false;
+                    }
+                }
+                else if (in_num == 0)
+                {
+                    mem_01.playerPos = go_player.transform.position;
+                    go_memoryObject.SetActive(true);
+                    go_worldSets[1].SetActive(true);
+                    go_worldSets[0].SetActive(false);
+                    RenderSettings.skybox = mt_memorySky;
+                    lt_memorySpot.intensity = 10;
+                    bl_fadeIn = true;
+                    in_num++;
+                }
+
+                if (bl_fadeIn)
+                {
+                    if (go_memoryObject != null) Camera.main.transform.LookAt(go_memoryObject.transform);
+
+                    float fl_fadeValue = (1 / fl_fadeDuration) * Time.deltaTime;
+                    Color cl_newFade = go_blackOut.GetComponent<Image>().color;
+
+                    if (go_blackOut.GetComponent<Image>().color.a > 0)
+                    {
+                        cl_newFade.a -= fl_fadeValue;
+                        go_blackOut.GetComponent<Image>().color = cl_newFade;
+                    }
+                    else
+                    {
+                        bl_fadeIn = false;
+                    }
+                }
+                else if (in_num == 1)
+                {
+                    while (bl_waitForAudio == false)
+                    {
+                        if (as_memory.clip != mem_01.sound) as_memory.clip = mem_01.sound;
+                        else
+                        {
+                            if (!as_memory.isPlaying) as_memory.Play();
+                            else bl_waitForAudio = true;
+                        }
+                    }
+
+                    if (bl_waitForAudio)
+                    {
+                        if (!as_memory.isPlaying)
+                        {
+                            float fl_fadeValue = (1 / fl_fadeDuration) * Time.deltaTime;
+                            Color cl_newFade = go_blackOut.GetComponent<Image>().color;
+
+                            if (go_blackOut.GetComponent<Image>().color.a < 1)
+                            {
+                                cl_newFade.a += fl_fadeValue;
+                                go_blackOut.GetComponent<Image>().color = cl_newFade;
+                            }
+                            else
+                            {
+                                go_memoryObject.SetActive(false);
+                                go_worldSets[0].SetActive(true);
+                                go_worldSets[1].SetActive(false);
+                                RenderSettings.skybox = null;
+                                lt_memorySpot.intensity = 0;
+
+                                go_door.transform.localPosition = new Vector3(0.139f, 1.988f, -1.686f);
+                                go_door.transform.localRotation = Quaternion.Euler(0, -75, 0);
+
+                                go_player.SetActive(false);
+                                go_player.transform.position = mem_01.playerPos;
+                                go_player.SetActive(true);
+
+                                in_num++;
+                            }
+                        }
+                        else if (go_memoryObject != null) Camera.main.transform.LookAt(go_memoryObject.transform);
+                    }
+                }
+                else if (in_num == 2)
+                {
+                    float fl_fadeValue = (1 / fl_fadeDuration) * Time.deltaTime;
+                    Color cl_newFade = go_blackOut.GetComponent<Image>().color;
+
+                    if (go_blackOut.GetComponent<Image>().color.a > 0)
+                    {
+                        cl_newFade.a -= fl_fadeValue;
+                        go_blackOut.GetComponent<Image>().color = cl_newFade;
+                    }
+                    else
+                    {
+                        bl_memComplete = false;
+                        GetComponent<CDM_PaintingPuzzleManager>().enabled = false;
+                    }
+                }
+            }
+
         }
     }
 }
